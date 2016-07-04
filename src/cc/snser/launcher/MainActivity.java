394188b7f1b1cbe11663fbf578b7b2131265e146
snser <snsermail@gmail.com>
2016-07-04 18:31:32 +0800

    ADD NET PROJECT

---
+++ src/cc/snser/launcher/MainActivity.java
@@ -0,0 +1,13 @@
+package cc.snser.launcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
